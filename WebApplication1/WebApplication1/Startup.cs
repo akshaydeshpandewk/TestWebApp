69b94cc5bf0c1b8c533f3209ff43716c2326b16c
Akshay Deshpande <Akshay.Deshpande@wolterskluwer.com>
2020-01-15 17:17:44 +0530

    q

--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
@@ -14,12 +16,25 @@ namespace WebApplication1
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
             Configuration = configuration;
+            HostingEnvironment = env;
+        }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
+        public IHostingEnvironment HostingEnvironment { get; }
+        private static readonly string APPSETTING_PATH = "appSettings.json";
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -33,6 +48,27 @@ namespace WebApplication1
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            if (Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"] != null)
+            {
+                string instrumentationkeyValue = (Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"]).ToString();
+
+
+                var filePath = Path.Combine(HostingEnvironment.WebRootPath, APPSETTING_PATH);
+                if (!File.Exists(filePath))
+                {
+                    filePath = Path.Combine(HostingEnvironment.ContentRootPath, APPSETTING_PATH);
+                }
+                if (File.Exists(filePath))
+                {
+                    dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(File.ReadAllText(filePath));
+                    jsonObj["ApplicationInsights:InstrumentationKey"] = new Guid("379D6A9A - 59A3 - 4341 - 896D - 9C8878261ED8");
+                    string output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
+                    File.WriteAllText(filePath, output);
+                }
+
+
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
